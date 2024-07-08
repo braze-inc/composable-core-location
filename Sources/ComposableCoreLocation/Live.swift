6d9aa7109205d78e93dfe30ba378579cfaeba876
Louis Bur <louisbur92@gmail.com>
2024-07-08 15:52:45 +0200

    Fix compilation on iOS 13.0

--- Sources/ComposableCoreLocation/Live.swift
+++ Sources/ComposableCoreLocation/Live.swift
@@ -210,6 +210,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
     self.continuation = continuation
   }
 
+  @available(iOS 14.0, tvOS 14.0, macOS 11.0, watchOS 7.0, *)
   func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
     self.continuation.yield(.didChangeAuthorization(manager.authorizationStatus))
   }
